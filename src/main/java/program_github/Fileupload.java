dd89f63d329daa5a79db06a42afb34fca473ad26
Dell <Dell@DESKTOP-P7V8EE3>
2024-06-04 21:39:15 +0530

    hello github

---
+++ src/main/java/program_github/Fileupload.java
@@ -0,0 +1,25 @@
+package program_github;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+public class Fileupload {
+	public static void main(String args[]) {
+		//System.setProperty("webdriver.chrome.driver", "C:\\Users\\Dell\\Downloads\\chromedriver.chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		driver.navigate().to("http://183.82.103.245/nareshit/login.php");
+      driver.findElement(By.name("txtUserName")).sendKeys("nareshit");
+      driver.findElement(By.name("txtPassword")).sendKeys("nareshit");
+      driver.findElement(By.name("Submit")).click();
+      driver.switchTo().frame("rightMenu");
+      driver.findElement(By.xpath("//input[@type='button'][@value='Add']")).click();
+      driver.findElement(By.name("txtEmpFirstName")).sendKeys("nareshit");
+      driver.findElement(By.name("txtEmpLastName")).sendKeys("testing");
+      WebElement fileinput =driver.findElement(By.xpath("//input[@name='photofile'][@id='photofile']"));
+      fileinput.sendKeys("C:\\Users\\Dell\\Downloads\\nature.jpg");
+      System.out.println("file upload");
+      driver.switchTo().defaultContent();
+      driver.close();
+
+}
+}
---
+++ src/main/java/program_github/practice_github.java
@@ -0,0 +1,12 @@
+package program_github;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class practice_github {
+public static void main(String[] args) {
+	WebDriver driver=new ChromeDriver();
+	driver.findElement(By.id(""));
+}
+}
